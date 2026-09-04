854d7d25a1f957c9201ea74ff591b29515732543
Andreas <aandreaspetrov@gmail.com>
2026-09-04 20:27:02 +0300

    config

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: 'icoffeehub-backend',
+      cwd: '/var/www/icoffeehub/backend',
+      script: 'dist/main.js',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 4000,
+      },
+    },
+
+    {
+      name: 'icoffeehub-frontend',
+      cwd: '/var/www/icoffeehub/frontend',
+      script: 'npm',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+  ],
+};
